d6c8c8a83369f52302321256953bd06cd15d91c3
ted <tedmarov@gmail.com>
2020-10-07 15:20:23 -0500

    Created FishList.js that appends header and container for holding list of fish to DOM

---
+++ scripts/FishDataProvider.js
@@ -0,0 +1,48 @@
+/*
+ *  To get you started, here's some properties of Bart.
+ *  You need to add more properties to complete his
+ *  representation as an object. Then add all the other
+ *  fish to the collection.
+ */
+const fishCollection = [
+    {
+        name: "Blue",
+        food: "Fish, squid, crustaceans.",
+        species: "T. albacares",
+        length: 35,
+        location: "Pacific Ocean (near Maui)",
+        image: "https://i.pinimg.com/originals/e2/54/e3/e254e37735f7f7b12b9c4ad1e8355156.jpg"
+    },
+    {
+        name: "Bubba",
+        food: "Worms, insect larvae, and some fish.",
+        species: "Nandopsis",
+        length: 8,
+        location: "Central American Caves (near Honduras)",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRNTe_oyGhdudKMTqPkLHYl2UijNPmdgmE_Yha54_f5REFW3-wv4Vz7xKK4BmV1st1fMlvdKl7lbd4VCH06ptS06chqTxXMA5frAbwj&usqp=CAU&ec=45707745"
+    },
+    {
+        name: "Barry",
+        food: "Fish, squid, crustaceans.",
+        species: "T. albacares",
+        length: 35,
+        location: "Pacific Ocean (near Maui)",
+        image: "https://i.pinimg.com/originals/e2/54/e3/e254e37735f7f7b12b9c4ad1e8355156.jpg"
+    },
+    {
+        name: "Buddy",
+        food: "Mollusks, crustaceans, and lakeweed.",
+        species: "Ambystoma mexicana",
+        length: 8,
+        location: "Harvest Location: Central American Caves (near Honduras)",
+        image: "https://i.pinimg.com/originals/30/33/2f/30332ff1599f61560272deb3d8f15163.jpg"
+    }
+]
+
+export const useFish = () => {
+    return fishCollection.slice()
+}
+
+export const addFish = (fishObject) => {
+    fishCollection.push(fishObject)
+}
---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+import { useFish } from './FishDataProvider.js'
+import { FishList } from './FishList.js'
+
+const allTheFish = useFish()
+
+/* console.log(all)
+
+for (const fish of allTheFish) {
+    console.log(fish)
+} */
+
+FishList()
