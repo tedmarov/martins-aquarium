0a96d9a66a647bc726a5031f001b3bea6e678a18
ted <tedmarov@gmail.com>
2020-10-07 22:48:30 -0500

    Fish added

---
+++ scripts/Fish.js
@@ -0,0 +1,12 @@
+export const Fish = (fishObj) => {
+    return `
+    <div class="fish__Blue">
+    <p class="fish__name">Name: ${fishObj.name}</p>
+    <img class="fish__image" src="${fishObj.image}" alt="">
+    <p class="fish__species">Species: ${fishObj.species}</p>
+    <p class="fish__length">Length: ${fishObj.length}</p>
+    <p class="fish__location">Harvest Location: ${fishObj.location}</p>
+    <p class="fish__diet">Diet: ${fishObj.food}</p>
+</div>
+    `   
+}
--- scripts/FishList.js
+++ scripts/FishList.js
@@ -4,17 +4,26 @@
 
 // TODO: Import `useFish` from the data provider module
 
+import { Fish } from "./Fish.js"
+import { useFish } from "./FishDataProvider.js"
+
 export const FishList = () => {
+    const contentElement = document.querySelector(".contentContainer__left")
 
-const contentElement = document.querySelector(".contentContainer__left")
+const fishes = useFish()
 
-contentElement.innerHTML += `
-<section class="fishList">
-<h3>Fish List</h3>
-<div class="fishContainer">
-Placeholder for list of fishes
-<div>
-</section>
+let fishHTMLRepresentations = ""
+for (const fish of fishes) {
+    fishHTMLRepresentations += Fish(fish)
+}
+
+    contentElement.innerHTML += `
+    <section class="fishList">
+    <h3>Fish List</h3>
+    <div class="fishContainer">
+        ${fishHTMLRepresentations}
+    </div>
+    </section>
 `
 
 }
